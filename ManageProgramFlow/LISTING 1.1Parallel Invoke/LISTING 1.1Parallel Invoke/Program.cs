cd67e11bbc35548c45be2581a3ef346544a58ab7
bornwarrior <j3patel@gmail.com>
2018-09-10 21:56:18 -0400

    Parellel Invoke Example

---
+++ ManageProgramFlow/LISTING 1.1Parallel Invoke/LISTING 1.1Parallel Invoke/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1._1Parallel_Invoke
+{
+    class Program
+    {
+        static void Task1()
+        {
+            Console.WriteLine($"Task 1 starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 1 ending");
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("Task 2 starting");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 2 Ending");
+        }
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() => Task1(), () => Task2());
+            Console.WriteLine("Finished procesing. Press a key to end");
+            Console.ReadKey();
+        }
+    }
+}
