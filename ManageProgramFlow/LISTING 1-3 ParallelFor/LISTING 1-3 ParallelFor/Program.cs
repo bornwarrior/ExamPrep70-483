a62474b5368d4ab7df4ae5583c6a951ef79d997f
bornwarrior <j3patel@gmail.com>
2018-09-11 05:36:11 -0400

    Threading Parellel For

---
+++ ManageProgramFlow/LISTING 1-3 ParallelFor/LISTING 1-3 ParallelFor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1_3_ParallelFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, 500).ToArray();
+            Parallel.For(0, items.Length, i =>
+             {
+                 WorkOnItem(items[i]);
+             });
+            Console.WriteLine("Main Finished Processing. Press a key to end.");
+            Console.ReadKey();
+        }
+
+        private static void WorkOnItem(object item)
+        {
+            Console.WriteLine($"Start working on {item} [{Thread.CurrentThread.ManagedThreadId}]");
+            Thread.Sleep(100);
+            Console.WriteLine($"Finished working on {item} [{Thread.CurrentThread.ManagedThreadId}]");
+        }
+    }
+}
