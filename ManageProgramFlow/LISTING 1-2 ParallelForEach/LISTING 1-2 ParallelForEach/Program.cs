a87af55f909448db4b2d6c2480da31808d17ae90
bornwarrior <j3patel@gmail.com>
2018-09-11 05:30:06 -0400

    Threading Parelll For Each

---
+++ ManageProgramFlow/LISTING 1-2 ParallelForEach/LISTING 1-2 ParallelForEach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1_2_ParallelForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, 500);
+            Parallel.ForEach(items, item => { WorkOnItem(item); });
+
+            Console.WriteLine("Finished processing. Press and a key to end");
+            Console.ReadKey();
+        }
+
+        private static void WorkOnItem(object item)
+        {
+            Console.WriteLine($"Started working on: {item}");
+            Thread.Sleep(100);
+            Console.WriteLine($"Finished working on: {item}");
+        }
+
+    }
+}
