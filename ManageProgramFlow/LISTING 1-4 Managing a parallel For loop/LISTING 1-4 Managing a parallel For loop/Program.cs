73244489cc26a5d11d32c56f3c31f5641df26b03
bornwarrior <j3patel@gmail.com>
2018-09-11 05:50:47 -0400

    Managing Parellel For Loop

---
+++ ManageProgramFlow/LISTING 1-4 Managing a parallel For loop/LISTING 1-4 Managing a parallel For loop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1_4_Managing_a_parallel_For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, 500).ToArray();
+
+            ParallelLoopResult result = Parallel.For(0, items.Count(), 
+            (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 200)
+                    loopState.Stop();
+
+                WorkOnItem(items[i]);
+            });
+        }
+
+        private static void WorkOnItem(object item)
+        {
+            Console.WriteLine($"Start working on {item} [{Thread.CurrentThread.ManagedThreadId}]");
+            Thread.Sleep(100);
+            Console.WriteLine($"Finished working on {item} [{Thread.CurrentThread.ManagedThreadId}]");
+        }
+    }
+}
