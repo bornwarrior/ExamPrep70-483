5da58d1701ca138895163966cfc3c8388a11248a
bornwarrior <j3patel@gmail.com>
2018-09-12 21:12:42 -0400

    Task Wait All

---
+++ ManageProgramFlow/LISTING 1-13 Task Returning A Value/LISTING 1-13 Task Returning A Value/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1_13_Task_Returning_A_Value
+{
+    class Program
+    {
+        public static int CalculateResult()
+        {
+            Console.WriteLine("Work Starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Work Finished");
+            return 99;
+        }
+        static void Main(string[] args)
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                return CalculateResult();
+            });
+
+            Console.WriteLine(task.Result);
+            Console.WriteLine("Finished processing. Press a key to end");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ManageProgramFlow/LISTING 1-14 Task Waitall/LISTING 1-14 Task Waitall/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1_14_Task_Waitall
+{
+    class Program
+    {
+        public static void DoWork(int i)
+        {
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Task {i} starting");
+            Thread.Sleep(2000);
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Task {i} finished");
+        }
+
+        static void Main(string[] args)
+        {
+            int taskCount = 10;
+            Task[] Tasks = new Task[taskCount];
+            for (int i = 0; i < taskCount; i++)
+            {
+                int taskNum = i;
+                Tasks[i] = Task.Run(() => DoWork(taskNum));
+            }
+            Task.WaitAll(Tasks);
+            Console.WriteLine("Finished processing. Press a key to end.");
+            Console.ReadKey();
+
+        }
+    }
+}
