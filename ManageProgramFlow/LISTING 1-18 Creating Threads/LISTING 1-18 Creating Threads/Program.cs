34b8ac3afea58104c728ce90759bcc6648fd5499
bornwarrior <j3patel@gmail.com>
2018-09-14 22:41:20 -0400

    Creating thread

---
+++ ManageProgramFlow/LISTING 1-18 Creating Threads/LISTING 1-18 Creating Threads/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LISTING_1_18_Creating_Threads
+{
+    class Program
+    {
+        static void ThreadHello()
+        {
+            Console.WriteLine($"Hello from the thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(2000);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            Thread thread = new Thread(ThreadHello);
+            thread.Start();
+        }
+    }
+}
