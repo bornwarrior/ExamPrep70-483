cf943c60b49b371e7625455deac9a28b98b74055
bornwarrior <j3patel@gmail.com>
2018-09-16 19:49:49 -0400

    Aborting a thread

---
+++ ManageProgramFlow/LISTING 1-23 Aborting Thread/LISTING 1-23 Aborting Thread/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LISTING_1_23_Aborting_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread tickThread = new Thread(() =>
+           {
+               while (true)
+               {
+                   Console.WriteLine("Tick");
+                   Thread.Sleep(1000);
+               }
+           });
+
+            tickThread.Start();
+            Console.WriteLine("Press a key to stop the clock");
+            Console.ReadKey();
+            tickThread.Abort();
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
