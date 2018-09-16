38c11904a9f527960f4dd505102eb73a0d7ed190
bornwarrior <j3patel@gmail.com>
2018-09-15 20:27:50 -0400

    Thread & Lambda

---
+++ ManageProgramFlow/LISTING 1-20 Threads And Lambda/LISTING 1-20 Threads And Lambda/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace LISTING_1_20_Threads_And_Lambda
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(() =>
+            {
+                Console.WriteLine($"Hello from the thread {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(1000);
+            });
+
+            thread.Start();
+            Console.WriteLine($"Main Press any key to end {Thread.CurrentThread.ManagedThreadId}");
+            Console.ReadKey();
+        }
+    }
+}
