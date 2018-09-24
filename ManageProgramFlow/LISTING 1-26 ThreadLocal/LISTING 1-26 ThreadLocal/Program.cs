abd1030627be31154e0844c4ab0ec0ff3b73da4b
bornwarrior <j3patel@gmail.com>
2018-09-23 20:36:04 -0400

    Thread Local

---
+++ ManageProgramFlow/LISTING 1-26 ThreadLocal/LISTING 1-26 ThreadLocal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LISTING_1_26_ThreadLocal
+{
+    class Program
+    {
+        public static ThreadLocal<Random> RandomGenerator =
+            new ThreadLocal<Random>(() =>
+           {
+               return new Random(2);
+           });
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() =>
+           {
+               for (int i = 0; i < 5; i++)
+               {
+                   Console.WriteLine($"t1[{Thread.CurrentThread.ManagedThreadId}]:{RandomGenerator.Value.Next(10)}");
+
+               }
+           });
+
+            Thread t2 = new Thread(() =>
+           {
+               for (int i = 0; i < 5; i++)
+               {
+                   Console.WriteLine($"t2[{Thread.CurrentThread.ManagedThreadId}] :{RandomGenerator.Value.Next(10)}");
+               }
+           });
+
+            t1.Start();
+            t2.Start();
+            Console.ReadKey();
+        }
+    }
+}
