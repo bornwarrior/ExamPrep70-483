46e9bc63fd2942e2e850a440c7169feeee911b58
bornwarrior <j3patel@gmail.com>
2018-09-24 21:04:06 -0400

    Thread Context

---
+++ ManageProgramFlow/LISTING 1-27 Thread context/LISTING 1-27 Thread context/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LISTING_1_27_Thread_context
+{
+    class Program
+    {
+        static void DisplayThread(Thread t)
+        {
+            Console.WriteLine($"Name: {t.Name}");
+            Console.WriteLine($"Culture: {t.CurrentCulture}");
+            Console.WriteLine($"Priority: {t.Priority}");
+            Console.WriteLine($"Execution Context: {t.ExecutionContext}");
+            Console.WriteLine($"IsBackground? :{t.IsBackground}");
+            Console.WriteLine($"IsPool?: {t.IsThreadPoolThread}");
+        }
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main Thread";
+            DisplayThread(Thread.CurrentThread);
+        }
+    }
+}
