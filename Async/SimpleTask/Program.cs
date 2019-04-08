010a1688d5d1e3ecf4e7c4e410c5cfd4c1537d7d
bornwarrior <j3patel@gmail.com>
2019-04-08 18:38:56 -0400

    Simple Aysnc Task Program

---
+++ Async/SimpleTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Repititions = 10000;
+
+            //hot async task control will return 
+            //and go to for
+            Task task = Task.Run(() =>
+              {
+                  for (int i = 0; i < Repititions; i++)
+                  {
+                      Console.Write("-");
+                  }
+              });
+
+            for (int count = 0; count < Repititions; count++)
+            {
+                Console.Write("+");
+            }
+
+            task.Wait();
+
+            Console.ReadKey();
+        }
+
+    }
+}
